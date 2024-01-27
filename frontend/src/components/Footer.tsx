d2f0ee9bcf50cb3213d9755508ebaf14db623aaf
Jesús Lorenzo Gallego <jelorenzogal@gmail.com>
2024-01-27 19:48:36 +0100

    update styles

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -6,6 +6,8 @@ export default function Footer() {
             <span>limited to <strong className="primary">3 files/10min</strong></span>
             <span>maximum <strong className="primary">100mb</strong> per file</span>
             <span>files are deleted <strong className="primary">5 min</strong> after upload</span>
+            <br />
+            <span style={{color: "#808080", fontStyle: "italic"}}>This is an open source project, check source <a href="https://github.com/chus-git/bitefim" target="_blank" title="Visit the GitHub repository">here</a></span>
         </footer>
     );
 
